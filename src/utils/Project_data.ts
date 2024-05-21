52f6ed94ba03bc375638fb724e513e75f374b539
Zobayer26 <zobayerarif126@gmail.com>
2024-05-21 20:49:44 +0600

    add project

--- src/utils/Project_data.ts
+++ src/utils/Project_data.ts
@@ -4,6 +4,7 @@ import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
 import project5_img from '../assets/project_5.png'
 import project6_img from '../assets/project_6.png'
+import project7_img from '../assets/project_7.png'
 
 const project_data = [
     {
@@ -15,60 +16,61 @@ const project_data = [
     },
     {
         w_no:2,
+        w_name:"Foody Delivery",
+        w_img:project7_img,
+        live:"https://food-delivery-lilac.vercel.app/",
+        github:"https://github.com/Zobayer26/Food_delivery_Assignment"
+    },
+    {
+        w_no:3,
         w_name:"Foody Zone",
         w_img:project2_img,
         live:"https://foody-zone-ivory.vercel.app/",
         github:"https://github.com/Zobayer26/Foody-Zone"
     },
     {
-        w_no:3,
+        w_no:4,
         w_name:"2048 Game",
         w_img:project3_img,
         live:"https://2048-game-hazel.vercel.app/",
         github:"https://github.com/Zobayer26/2048-Game"
     },
     {
-        w_no:4,
+        w_no:5,
         w_name:"RedStrore landing page",
         w_img:project4_img,
         live:"https://redstore-landing-page.vercel.app/",
         github:"https://github.com/Zobayer26/Redstore-landing-page"
     },
     {
-        w_no:5,
+        w_no:6,
         w_name:"Candy Crush",
         w_img:project5_img,
         live:"https://candy-crush-seven.vercel.app/",
         github:"https://github.com/Zobayer26/candy-crush"
     },
     {
-        w_no:6,
+        w_no:7,
         w_name:"Css mentor task",
         w_img:project6_img,
         live:"css-mentor-five.vercel.app",
         github:"https://github.com/Zobayer26/css-mentor"
     },
     {
-        w_no:7,
+        w_no:8,
         w_name:"RedStrore landing page",
         w_img:project4_img,
         live:"redstore-landing-page.vercel.app",
         github:"https://github.com/Zobayer26/Redstore-landing-page"
     },
     {
-        w_no:8,
+        w_no:9,
         w_name:"Candy Crush",
         w_img:project5_img,
         live:"candy-crush-liard.vercel.app",
         github:"https://github.com/Zobayer26/candy-crush"
     },
-    {
-        w_no:9,
-        w_name:"Css mentor task",
-        w_img:project6_img,
-        live:"css-mentor-five.vercel.app",
-        github:"https://github.com/Zobayer26/css-mentor"
-    },
+
 ]
  
 export default project_data;
